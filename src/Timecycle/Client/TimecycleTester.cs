252cee3bc2434a289de23ce69a420dba50362db0
dave <dub.cyclone@gmail.com>
2019-01-17 12:31:06 +0000

    Added small client to test timecyle modifiers.

---
+++ src/Timecycle/Client/TimecycleTester.cs
@@ -0,0 +1,62 @@
+﻿using RAGE;
+using RAGE.Game;
+using RageMpClientHelpers;
+using RageMpHelper;
+using System.Collections.Generic;
+
+namespace Timecycle
+{
+    public class TimecycleTester : Events.Script
+    {
+        private bool _on;
+        private int _index = 1;
+
+        public TimecycleTester()
+        {
+            GfxHelper.ClearCycle();
+            Events.Tick += OnTick;
+
+            _on = false;
+        }
+
+        private void OnTick(List<Events.TickNametagData> nametags)
+        {
+            if (Pad.IsControlJustPressed(0, (int)Control.ReplayStartStopRecordingSecondary))
+            {
+                UiHelper.ShowSubtitle("key pressed", 1000);
+                if (!_on)
+                {
+                    _on = true;
+                    GfxHelper.SetTimeCycleById(_index);
+                }
+                else
+                {
+                    GfxHelper.ClearCycle();
+                    _on = false;
+                }
+            }
+
+            if (Pad.IsControlJustPressed(0, (int)Control.PhoneLeft))
+            {
+                _index--;
+                UpdateIndex();
+            }
+                
+            else if (Pad.IsControlJustPressed(0, (int)Control.PhoneRight))
+            {
+                _index++;
+                UpdateIndex();
+            }                
+        }
+
+        private void UpdateIndex()
+        {
+            if (_index > 774)
+                _index = 1;
+            else if (_index < 1)
+                _index = 774;
+
+            UiHelper.ShowSubtitle($"Cycle: {GfxHelper.SetTimeCycleById(_index)}, ID:{_index}", 2500);
+        }
+    }
+}
---
+++ src/Timecycle/Readme.md
@@ -0,0 +1,13 @@
+# <span style="color:#018144">RAGEMP - Timecycle Tester</span>
+
+---
+
+## Description
+
+Small client to test timecycles. 1 - 774
+
+### Keys:
+
+- <kbd>F2</kbd> = Enable timecycles
+- <kbd>←</kbd>	= Prev TC index
+- <kbd>→</kbd>	= Next TC index
